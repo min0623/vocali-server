2b2c607c1316161399ed6fce288cc28733c641e9
min0623 <confuoco1023@gmail.com>
2021-06-02 23:55:04 +0900

    Update model API endpoint

--- src/main/kotlin/com/cs492e/vocali/controller/UserController.kt
+++ src/main/kotlin/com/cs492e/vocali/controller/UserController.kt
@@ -19,7 +19,7 @@ import kotlin.math.min
 class UserController {
 
     companion object {
-        const val MODEL_URL = "http://c86cb0e41ebd.ngrok.io"
+        const val MODEL_URL = "http://a45e5a6fdfa7.ngrok.io"
     }
 
     @Autowired
